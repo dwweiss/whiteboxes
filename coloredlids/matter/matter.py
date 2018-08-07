7bca650be0db89a4f911a9d0e8407ac49a7a9bec
Dietmar W. Weiss <dwweiss@users.noreply.github.com>
2018-08-07 11:09:41 +0200

    Update matter.py

--- coloredlids/matter/matter.py
+++ coloredlids/matter/matter.py
@@ -17,66 +17,24 @@
   02110-1301 USA, or see the FSF site: http://www.fsf.org.
 
   Version:
-      2018-06-25 DWW
+      2018-08-07 DWW
 """
 
 from grayboxes.base import Base
 
 from . parameter import C2K
-from . nonmetals import NonMetals
-from . nonferrousmetals as NonFerrousMetals
 from . ferrousmetals as FerrousMetals
+from . nonferrousmetals as NonFerrousMetals
+from . nonmetals import NonMetals
 from . liquids import Liquids
 from . gases import Gases
-
-
-def molecularWeight(identifier):
-    """
-    Args:
-        identifier (string):
-            Element or organic compound
-
-    Returns:
-        (float):
-            Molecular weight (molar mass) in [kg/mol]
-
-    Reference:
-        https://www.lenntech.com/calculators/molecular/
-        molecular-weight-calculator.htm
-    """
-    first = identifier[0].upper()
-    if first == 'A':
-        if   identifier == 'Ar':     return 39.95e-3
-    elif first == 'C':
-        if   identifier == 'C':      return 12.01e-3
-        elif identifier == 'CO2':    return 44.01e-3
-        elif identifier == 'CH4':    return 16.04e-3
-        elif identifier == 'C2H4':   return 28.05e-3
-        elif identifier == 'C2H5OH': return 12.01e-3
-        elif identifier == 'C2H6':   return 30.07e-3
-        elif identifier == 'C3H6':   return 42.07e-3
-        elif identifier == 'C3H8':   return 44.01e-3
-        elif identifier == 'C4H10':  return 58.12e-3
-    elif first == 'H':
-        if   identifier =='H':       return 1.008e-3
-        elif identifier =='He':      return 4.003e-3
-        elif identifier =='H2O':     return 18.02e-3
-        elif identifier =='H2S':     return 34.08e-3
-    elif first == 'N':
-        if identifier =='N2':        return 28.01e-3
-    elif first == 'O':
-        if   identifier =='O':       return 16.00e-3
-        elif identifier =='O2':      return 32.00e-3
-
-    assert 0, 'identifier:' + str(identifier)
-    return None
 
 
 class Matter(Base):
     """
     Collection of physical and chemical properties of matter
 
-    Intended to be a follower of root in a Base-based tree
+    Designed as follower in tree of white box models 
 
     Args:
         identifier (string, optional):
@@ -134,7 +92,7 @@ if __name__ == '__main__':
         print('-' * len(s) + '\n' + s + '\n' + '-' * len(s))
 
         collection = Matter()
-        print('Collection:', collection)  # dict_keys(['304','Ryton','Water'])
+        print('Collection:', collection)
         mat = collection('water')
         mat.plot('c_p')
 
