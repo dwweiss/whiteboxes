0b3f8e698e061dab5e9df97c1b39e243437e5301
Dietmar W. Weiss <dwweiss@users.noreply.github.com>
2018-10-15 11:23:31 +0200

    Add files via upload

---
+++ tests/test_inrange.py
@@ -0,0 +1,50 @@
+"""
+  Copyright (c) 2016- by Dietmar W Weiss
+
+  This is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation; either version 3.0 of
+  the License, or (at your option) any later version.
+
+  This software is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this software; if not, write to the Free
+  Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+  02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+  Version:
+      2018-10-15 DWW
+"""
+
+import unittest
+import sys
+import os
+
+sys.path.append(os.path.abspath('..'))
+from coloredlids.data.inrange import in_range
+
+
+class TestUM(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test1(self):
+        print('in_range:', in_range(2, [3, 4]))
+        print('in_range:', in_range(None, [1, 4]))
+        print('in_range:', in_range(2, [1, 4]))
+        print('in_range:', in_range(2, [3, 4], 3, (2, 9)))
+        print('in_range:', in_range(2, [1, 4], 3, (None, 9), 3, (None, None)))
+        print('in_range:', in_range(None, [1, 4], None, (None, 9), None, 
+                                    (7, 0)))
+        self.assertTrue(True)
+
+
+if __name__ == '__main__':
+    unittest.main()
